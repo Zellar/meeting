81926932656dceef7e99388c71c65b96cbf403e2
Denis Samohodkin <Danielmag@mail.ru>
2021-08-25 15:12:45 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".header__mobile-btn").onclick = function() {
-    open();
-  };
-  
-  function open() {
-    document.querySelector(".header__nav").classList.toggle("show");
-    document.querySelector(".header__mobile-btn").classList.toggle("show");
-  }
